327fd7f912ea9e893b4db6ee2d9b6ee64baff99e
InYoung Park <dlsdudg15@naver.com>
2022-08-22 16:36:34 +0900

    Create mouse_draw.py

---
+++ pythons/opencv_init/mouse_draw.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+old_x = old_y = -1
+
+
+def on_mouse(event, x, y, flags, user_data):
+    global old_x, old_y
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        old_x, old_y = x, y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if flags & cv2.EVENT_FLAG_LBUTTON:
+            cv2.line(img, (old_x, old_y), (x, y), (0, 0, 255), 5, cv2.LINE_AA)
+            cv2.imshow("image", img)
+            old_x, old_y = x, y
+
+
+img = np.ones((480, 640, 3), dtype=np.uint8) * 255
+cv2.imshow("image", img)
+cv2.setMouseCallback("image", on_mouse, img)
+cv2.waitKey()
