aa54a2b6e2b04f794ab19e51f45595ec2fd3059e
InYoung Park <dlsdudg15@naver.com>
2022-08-24 14:26:25 +0900

    Create stitch.py

---
+++ pythons/opencv_init/stitch.py
@@ -0,0 +1,24 @@
+import sys
+import cv2
+
+img_names = ["images/pano1.jpg", "images/pano2.jpg", "images/pano3.jpg"]
+imgs = []
+
+for name in img_names:
+    img = cv2.imread(name)
+
+    if img is None:
+        sys.exit()
+
+    imgs.append(img)
+
+stitcher = cv2.Stitcher_create()
+status, dst = stitcher.stitch(imgs)
+
+if status != cv2.STITCHER_OK:
+    sys.exit()
+
+cv2.imwrite("images/output.jpg", dst)
+cv2.imshow("dst", dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
