2de9878a18483374f7fc314b8b10b31b600402f9
InYoung Park <dlsdudg15@naver.com>
2022-08-24 11:01:59 +0900

    Create contours.py

---
+++ pythons/opencv_init/contours.py
@@ -0,0 +1,28 @@
+# 외곽선 검출
+from re import I
+import cv2
+import sys
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+src = cv2.imread("images/OpenCV_logo.png", cv2.IMREAD_GRAYSCALE)
+
+if src is None:
+    print("Image load failed!")
+    sys.exit()
+
+# contour 윤곽
+contours, hierarchy = cv2.findContours(src, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
+
+idx = 0
+while idx >= 0:
+    c = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    cv2.drawContours(dst, contours, idx, c, 2, cv2.LINE_8, hierarchy)
+    idx = hierarchy[0][idx][0]
+
+cv2.imshow("src", src)
+cv2.imshow("dst", dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
