7d8f2e8c596f71131ed7153829bf28a5f08fcba0
InYoung Park <dlsdudg15@naver.com>
2022-08-25 16:58:59 +0900

    Update __init__.py

--- pythons/src/flask/flast_chat/app/__init__.py
+++ pythons/src/flask/flast_chat/app/__init__.py
@@ -21,6 +21,18 @@ def create_app():
     def index():
         return render_template("index.html")
 
+    @app.route("/set-user")
+    def set_user():
+        data = models.UserInfo(name="홍길동")
+        db.session.add(data)
+        db.session.commit()
+        return "ok"
+
+    @app.route("/get-user")
+    def get_user():
+        data = models.UserInfo.query.get(1)
+        return str(data.name)
+
     @app.route("/chat")
     def chat():
         return {"result": "Hello World!"}
