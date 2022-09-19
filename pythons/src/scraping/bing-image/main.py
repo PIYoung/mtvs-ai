5a420a1b91c734ec52bc0a68742ffdca6a1c9c65
InYoung Park <dlsdudg15@naver.com>
2022-09-19 09:31:35 +0900

    Update main.py

--- pythons/src/scraping/bing-image/main.py
+++ pythons/src/scraping/bing-image/main.py
@@ -1,11 +1,14 @@
 from bing_image_downloader import downloader
 
-query_string = "우영우"
-downloader.download(
-    query_string,
-    limit=10,
-    output_dir="images",
-    adult_filter_off=True,
-    force_replace=False,
-    timeout=60,
-)
+EMOTIONS_LIST = ["Angry Face", "Disgust Face", "Fear Face", "Neutral Face", "Sad Face", "Surprise Face"]
+
+for emotion in EMOTIONS_LIST:
+    query_string = emotion
+    downloader.download(
+        query_string,
+        limit=100,
+        output_dir="images",
+        adult_filter_off=True,
+        force_replace=False,
+        timeout=60,
+    )
